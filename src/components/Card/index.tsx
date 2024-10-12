17b5f2175dbc8ff8d391a760b245b607833e4fdd
ahang <2283582431@qq.com>
2024-10-12 20:55:58 +0800

    卡片组件

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,65 @@
+import { View, Image } from '@tarojs/components'
+import './index.scss'
+
+interface CardProps{
+  // ?叫做可选类型
+  title?:string,
+  content?:any
+  confirmBtn?:string
+  cancelBtn?:string
+  changeName:()=>void
+}
+
+
+//  1.父组件传值给子组件
+export default function Card({title,content,confirmBtn,cancelBtn,changeName}:CardProps) {
+  return (
+    <View className='card'>
+      <View className='header'>
+        <Image className='img' src='https://v4.mui.com/static/images/cards/contemplative-reptile.jpg' mode='widthFix'></Image>
+      </View>
+      <View className='main'>
+        <View className='title'>
+          {/* ?? || 逻辑运算符 */}
+          {title || '这是默认值'}
+        </View>
+        <View className='content'>
+          {content}
+        </View>
+      </View>
+      <View className='footer'>
+        <View className='btn-action' onClick={()=>changeName()}>
+          {confirmBtn || '确定'}
+        </View>
+        <View className='btn-action'>
+          {cancelBtn || '取消'}
+        </View>
+      </View>
+    </View>
+  )
+}
+
+// 2.箭头函数写法
+// const Card:React.FC<CardProps> = ({title})=>{
+//   return (
+//     <View className='card'>
+//     <View className='header'>
+//       <Image className='img' src='https://v4.mui.com/static/images/cards/contemplative-reptile.jpg' mode='widthFix'></Image>
+//     </View>
+//     <View className='main'>
+//       <View className='title'>
+//         {title}
+//       </View>
+//       <View className='content'>
+//         Lorem ipsum dolor sit amet, consectetur enim explicabo nulla quidem voluptatem quisquam architecto nostrum, numquam.
+//       </View>
+//     </View>
+//     <View className='footer'>
+//       <View className='btn-action'>SHARE</View>
+//       <View className='btn-action'>LEARN MORE</View>
+//     </View>
+//   </View>
+//   )
+// }
+
+// export default Card
